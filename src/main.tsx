b0350746e665b321c998264fb007a3e6c61352c1
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-06-07 22:12:51 +0100

    feat: fixes

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import ModalProvider from './entry/context/ModalProvider.tsx';
 import AppProvider from './entry/context/AppProvider.tsx';
+import env from './utils/env.ts';
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -14,9 +15,11 @@ const queryClient = new QueryClient({
     },
 });
 
+const url = `http://${env.IS_TESTNET ? 'testnet.' : ''}quyx.xyz/manifest/tonconnect.json`;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <TonConnectUIProvider manifestUrl="http://localhost:5173/manifest/tonconnect.json">
+        <TonConnectUIProvider manifestUrl={url}>
             <QueryClientProvider client={queryClient}>
                 <ModalProvider>
                     <AppProvider>
