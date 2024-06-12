9d0589132ea6eb1d4196fb8c886bd3e7e68ce575
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-06-12 12:53:23 +0100

    feat: changes

--- src/entry/pages/Explore/Explore.tsx
+++ src/entry/pages/Explore/Explore.tsx
@@ -3,7 +3,7 @@ import useApi from '../../hooks/useApi';
 
 const Explore = () => {
     const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [nft, setNfts] = useState<NftItem[]>([]);
+    const [nfts, setNfts] = useState<NftItem[]>([]);
 
     useEffect(() => {
         (async function () {
@@ -15,7 +15,35 @@ const Explore = () => {
         })();
     }, []);
 
-    return <>{isLoading ? 'loading...' : JSON.stringify(nft)}</>;
+    return (
+        <>
+            <div className="bg" />
+
+            {isLoading ? (
+                <div
+                    style={{ padding: '1rem', gap: '1.5rem', height: '60dvh' }}
+                    className="d-flex flex-column align-items-center justify-content-center"
+                >
+                    <span className="loader-span-sm"></span>
+                    <p style={{ opacity: 0.5 }}>Loading...</p>
+                </div>
+            ) : nfts.length == 0 ? (
+                'nothing found'
+            ) : (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="px-2 py-5">
+                                <div className="col-12">
+                                    <div className="row"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default Explore;
